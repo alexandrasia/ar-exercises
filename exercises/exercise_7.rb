68e2a1e83718f5bbbb168dc64d11fe2f3245ecc7
Alexandra Sia <alexandrasia@gmail.com>
2017-06-06 19:05:14 -0700

    Exercise 7: Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,30 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_wage, numericality: { greater_than: 40, less_than: 200 }
+  validates :store_id, presence: true
+end
+
+class Store < ActiveRecord::Base
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  validate :must_have_mens_or_womens_apparel
+
+  def must_have_mens_or_womens_apparel
+    if (!mens_apparel.present? && !womens_apparel.present?)
+      errors.add(:mens_or_womens_apparel, "must be present")
+    end
+  end
+end
+
+puts "Please enter a store name"
+@store_name = gets.chomp
+
+@store = Store.create(name: @store_name)
+
+puts @store.valid?
+puts @store.errors.messages
+
